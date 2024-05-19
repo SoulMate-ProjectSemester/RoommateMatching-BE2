d7ac82cbb916408a9fdcf3f782a925500b3de1fd
vkflco08 <vkflco08@naver.com>
2024-05-19 18:12:37 +0900

    update dockerfile

--- gpt_api/roomController.js
+++ gpt_api/roomController.js
@@ -8,7 +8,7 @@ exports.handleRoom = async (req, res) => {
     }
   
     try {
-      //await writeFile.saveToRoomMessageFile(roomId);
+      await writeFile.saveToRoomMessageFile(roomId);
       const message = "방에 있는 사람들이 서로에 대해 선호도가 얼마나 되는지 1 - 10점으로 알려줘.";
       const response = await roomService.getRoomResponse(roomId, message);
       res.json(response);
