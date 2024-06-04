91d7d82c3bcc9955398ff2db6210ae30e4bbb9c4
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2024-06-05 00:33:30 +0900

    feat : initialize 함수 다시 빼기

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const chatController = require('./gpt_api/chatController');
 const roomController = require('./gpt_api/roomController');
-const initialize = require('./initialize');
 const app = express();
 const port = process.env.PORT || 8181;
 const cors = require('cors');
 
-initialize();  // 애플리케이션 시작 시 함수 실행
 
 app.use(cors());
 app.use(express.json());
