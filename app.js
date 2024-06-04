04fa4009aab7caba8c2190282a89b71bcc485172
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2024-06-05 00:24:55 +0900

    feat : 모든 port에 대해 접근 허용

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const chatController = require('./gpt_api/chatController');
 const roomController = require('./gpt_api/roomController');
 const app = express();
 const port = process.env.PORT || 8181;
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -12,5 +14,5 @@ app.post('/chat', chatController.handleChat);
 app.post('/room', roomController.handleRoom);
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://soulmate.pe.kr:${port}`);
 });
